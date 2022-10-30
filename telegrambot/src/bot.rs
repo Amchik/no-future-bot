09c82ef0770b3c5d85dece42afa57343c6bce086
ValgrindLLVM <valgrindllvm@proton.me>
2022-10-30 17:30:25 +0300

    [telegrambot] Add /purgeadmins command

--- telegrambot/src/bot.rs
+++ telegrambot/src/bot.rs
@@ -1,4 +1,4 @@
-use sea_orm::DatabaseConnection;
+use sea_orm::{sea_query::Expr, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter, Value};
 use teloxide::{
     prelude::*,
     types::{InlineKeyboardButton, MessageKind, ReplyMarkup, WebAppInfo},
@@ -27,10 +27,22 @@ pub async fn start_bot(bot: Bot, db: DatabaseConnection) {
 enum Command {
     #[command(description = "show this message.")]
     Start,
+    #[command(description = "purges all administrators of channel, except you.")]
+    PurgeAdmins,
 }
 
-async fn commands_handler(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    if let MessageKind::Common(_) = msg.kind {
+async fn commands_handler(
+    bot: Bot,
+    msg: Message,
+    db: DatabaseConnection,
+    cmd: Command,
+) -> ResponseResult<()> {
+    if let MessageKind::Common(common_message) = msg.kind {
+        let user = match common_message.from {
+            Some(u) => u,
+            None => return Ok(()),
+        };
+
         match cmd {
             Command::Start => {
                 bot.send_message(msg.chat.id, Command::descriptions().to_string())
@@ -41,6 +53,48 @@ async fn commands_handler(bot: Bot, msg: Message, cmd: Command) -> ResponseResul
                         },
                     )]]))
                     .await?;
+            }
+            Command::PurgeAdmins => {
+                let model = entity::telegram_user::Entity::find_by_id(user.id.0 as i64)
+                    .one(&db)
+                    .await
+                    .unwrap();
+                let chat_id = match model {
+                    Some(entity::telegram_user::Model {
+                        channel: Some(channel),
+                        ..
+                    }) => channel,
+                    _ => {
+                        bot.send_message(msg.chat.id, "You don't have linked channel")
+                            .await?;
+
+                        return Ok(());
+                    }
+                };
+                let member = bot.get_chat_member(ChatId(chat_id), user.id).await?;
+                if !member.is_owner() {
+                    bot.send_message(msg.chat.id, "You aren't the owner of linked chat")
+                        .await?;
+
+                    return Ok(());
+                }
+
+                let res = entity::telegram_user::Entity::update_many()
+                    .col_expr(
+                        entity::telegram_user::Column::Channel,
+                        Expr::value(Value::String(None)),
+                    )
+                    .filter(entity::telegram_user::Column::Channel.eq(Some(chat_id)))
+                    .filter(entity::telegram_user::Column::Id.ne(user.id.0 as i64))
+                    .exec(&db)
+                    .await
+                    .unwrap();
+
+                bot.send_message(
+                    msg.chat.id,
+                    format!("Purged {} administrators", res.rows_affected),
+                )
+                .await?;
             }
         };
     }
